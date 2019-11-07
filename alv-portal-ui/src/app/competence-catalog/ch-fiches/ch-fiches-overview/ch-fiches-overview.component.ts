e093d8cc885de0883eca47a1622c8b73820698fe
ganqqwerty <katkov.juriy@gmail.com>
2019-11-07 15:10:57 +0100

    merge

--- alv-portal-ui/src/app/competence-catalog/ch-fiches/ch-fiches-overview/ch-fiches-overview.component.ts
+++ alv-portal-ui/src/app/competence-catalog/ch-fiches/ch-fiches-overview/ch-fiches-overview.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OccupationSuggestionService } from '../../../shared/occupations/occupation-suggestion.service';
+import { AuthenticationService } from '../../../core/auth/authentication.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ChFicheRepository } from '../../../shared/backend-services/competence-catalog/ch-fiche/ch-fiche.repository';
+import { OverviewComponent } from '../../shared/overview/overview.component';
+import { ChFiche } from '../../../shared/backend-services/competence-catalog/ch-fiche/ch-fiche.types';
+import { OccupationTypeaheadItem } from '../../../shared/occupations/occupation-typeahead-item';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'alv-ch-fiches-overview',
@@ -6,6 +15,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./ch-fiches-overview.component.scss']
 })
 export class ChFichesOverviewComponent extends OverviewComponent<ChFiche> implements OnInit {
+
+  searchForm: FormGroup;
+
+  loadOccupationsFn = this.loadOccupations.bind(this);
 
 
   constructor(protected itemsRepository: ChFicheRepository,
@@ -19,8 +32,15 @@ export class ChFichesOverviewComponent extends OverviewComponent<ChFiche> implem
 
   ngOnInit() {
     super.ngOnInit();
+    this.searchForm = this.fb.group({
+      query: [''],
+      occupations: ['']
+    });
   }
 
+  loadOccupations(query: string): Observable<OccupationTypeaheadItem[]> {
+    return this.occupationSuggestionService.fetchCompetenceCatalogOccupations(query);
+  }
 
   editChFiche(chFiche: ChFiche) {
     this.router.navigate(['edit', chFiche.id], { relativeTo: this.route });
