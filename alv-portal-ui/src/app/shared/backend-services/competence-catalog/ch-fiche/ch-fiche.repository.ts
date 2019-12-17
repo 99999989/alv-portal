5a9bd352744ee31e6143bd34d3b53ab0aaa35338
Iurii Katkov <iurii.katkov@mimacom.com>
2019-12-17 16:07:06 +0100

    DF-1892 fiche repo triage

--- alv-portal-ui/src/app/shared/backend-services/competence-catalog/ch-fiche/ch-fiche.repository.ts
+++ alv-portal-ui/src/app/shared/backend-services/competence-catalog/ch-fiche/ch-fiche.repository.ts
@@ -6,48 +6,55 @@ import { createPageableURLSearchParams, PagedSearchRequest } from '../../request
 import { Page } from '../../shared.types';
 import { ChFiche, CreateChFiche, UpdateChFiche } from './ch-fiche.types';
 import { SearchService } from '../search-service';
-import { KK_EDITOR_ENDPOINT } from '../endpoints';
+import { TriageService } from '../triage.service';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ChFicheRepository implements SearchService<ChFiche> {
 
-  private readonly resourceUrl = KK_EDITOR_ENDPOINT + '/api/ch-fiches/';
+  private readonly resourceUrl = '/api/ch-fiches/';
 
   private readonly searchUrl = `${this.resourceUrl}_search`;
 
   private readonly findUrl = `${this.resourceUrl}_find`;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              public triageService: TriageService) {
   }
 
   findById(id: string): Observable<ChFiche> {
-    return this.http.get<ChFiche>(this.resourceUrl + id);
+    return this.triageService.endpoint$.pipe(
+      switchMap(endpoint => this.http.get<ChFiche>(endpoint + this.resourceUrl + id))
+    );
   }
 
   search(request: PagedSearchRequest): Observable<Page<ChFiche>> {
 
     const params = createPageableURLSearchParams(request);
-    return this.http.post<Page<ChFiche>>(this.searchUrl, request.body, {
-      params
-    });
+    return this.triageService.endpoint$.pipe(
+      switchMap(endpoint => this.http.post<Page<ChFiche>>(endpoint + this.searchUrl, request.body, {
+        params
+      }))
+    );
   }
 
   findByCompetenceSetId(competenceSetId: string): Observable<ChFiche[]> {
-    return this.http.get<ChFiche[]>(this.findUrl + '/byCompetenceSetId', {
-      params: new HttpParams().set('id', competenceSetId)
-    });
+    return this.triageService.endpoint$.pipe(
+      switchMap(endpoint => this.http.get<ChFiche[]>(endpoint + this.findUrl + '/byCompetenceSetId', {
+        params: new HttpParams().set('id', competenceSetId)
+      })));
   }
 
   create(chFiche: CreateChFiche): Observable<ChFiche> {
-    return this.http.post<ChFiche>(this.resourceUrl, chFiche);
+    return this.triageService.endpoint$.pipe(switchMap(endpoint => this.http.post<ChFiche>(endpoint + this.resourceUrl, chFiche)));
   }
 
   update(id: string, chFiche: UpdateChFiche): Observable<ChFiche> {
-    return this.http.put<ChFiche>(this.resourceUrl + id, chFiche);
+    return this.triageService.endpoint$.pipe(switchMap(endpoint => this.http.put<ChFiche>(endpoint + this.resourceUrl + id, chFiche)));
   }
 
   delete(id: string): Observable<void> {
-    return this.http.delete<void>(this.resourceUrl + id);
+    return this.triageService.endpoint$.pipe(switchMap(endpoint => this.http.delete<void>(endpoint + this.resourceUrl + id)));
   }
 
 }
