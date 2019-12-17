3a25dc1ceffcbe588d5687bc59ca05d8e7752ff1
Iurii Katkov <iurii.katkov@mimacom.com>
2019-12-17 16:25:47 +0100

    DF-1892 kk-set triage

--- alv-portal-ui/src/app/shared/backend-services/competence-catalog/competence-set/competence-set.repository.ts
+++ alv-portal-ui/src/app/shared/backend-services/competence-catalog/competence-set/competence-set.repository.ts
@@ -9,64 +9,84 @@ import {
   CreateCompetenceSet,
   UpdateCompetenceSet
 } from './competence-set.types';
-import { flatMap, map } from 'rxjs/operators';
+import { flatMap, map, switchMap } from 'rxjs/operators';
 import { CompetenceElementRepository } from '../competence-element/competence-element.repository';
 import { SearchService } from '../search-service';
-import { KK_EDITOR_ENDPOINT } from '../endpoints';
+import { TriageService } from '../triage.service';
 
 @Injectable({ providedIn: 'root' })
 export class CompetenceSetRepository implements SearchService<CompetenceSetSearchResult> {
 
-  private readonly resourceUrl = KK_EDITOR_ENDPOINT + '/api/competence-sets/';
+  private readonly resourceUrl = '/api/competence-sets/';
 
   private readonly searchUrl = `${this.resourceUrl}_search`;
 
   private readonly findUrl = `${this.resourceUrl}_find`;
 
   constructor(private http: HttpClient,
-              private competenceElementRepository: CompetenceElementRepository) {
+              private competenceElementRepository: CompetenceElementRepository,
+              public triageService: TriageService) {
+
   }
 
   findById(id: string): Observable<CompetenceSetSearchResult> {
-    return this.http.get<CompetenceSet>(this.resourceUrl + id).pipe(
-      flatMap(competenceSet => {
-        return this.competenceElementRepository.findById(competenceSet.knowHowId).pipe(
-          map(competenceElement => {
-            return <CompetenceSetSearchResult>{
-              id: competenceSet.id,
-              competenceElementIds: competenceSet.competenceElementIds,
-              knowHow: competenceElement,
-              draft: competenceSet.draft,
-              published: competenceSet.published
-            };
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.get<CompetenceSet>(endpoint + this.resourceUrl + id).pipe(
+          flatMap(competenceSet => {
+            return this.competenceElementRepository.findById(competenceSet.knowHowId).pipe(
+              map(competenceElement => {
+                return <CompetenceSetSearchResult>{
+                  id: competenceSet.id,
+                  competenceElementIds: competenceSet.competenceElementIds,
+                  knowHow: competenceElement,
+                  draft: competenceSet.draft,
+                  published: competenceSet.published
+                };
+              })
+            );
           })
-        );
-      })
-    );
+        )
+      ));
   }
 
   findByCompetenceElementId(competenceElementId: string): Observable<CompetenceSetSearchResult[]> {
-    return this.http.get<CompetenceSetSearchResult[]>(`${this.findUrl}/byCompetenceElementId`, {
-      params: new HttpParams().set('id', competenceElementId)
-    });
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.get<CompetenceSetSearchResult[]>(endpoint + `${this.findUrl}/byCompetenceElementId`, {
+          params: new HttpParams().set('id', competenceElementId)
+        })
+      ));
   }
 
   search(request: PagedSearchRequest): Observable<Page<CompetenceSetSearchResult>> {
     const params = createPageableURLSearchParams(request);
-    return this.http.post<Page<CompetenceSetSearchResult>>(this.searchUrl, request.body, {
-      params
-    });
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.post<Page<CompetenceSetSearchResult>>(endpoint + this.searchUrl, request.body, {
+          params
+        })
+      ));
   }
 
   create(competenceSet: CreateCompetenceSet): Observable<CompetenceSet> {
-    return this.http.post<CompetenceSet>(this.resourceUrl, competenceSet);
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.post<CompetenceSet>(endpoint + this.resourceUrl, competenceSet)
+      ));
   }
 
   update(id: string, competenceSet: UpdateCompetenceSet): Observable<CompetenceSet> {
-    return this.http.put<CompetenceSet>(`${this.resourceUrl}${id}`, competenceSet);
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.put<CompetenceSet>(endpoint + `${this.resourceUrl}${id}`, competenceSet)
+      ));
   }
 
   delete(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.resourceUrl}${id}`);
+    return this.triageService.endpoint$.pipe(
+      switchMap(
+        endpoint => this.http.delete<void>(endpoint + `${this.resourceUrl}${id}`)
+      ));
   }
 }
