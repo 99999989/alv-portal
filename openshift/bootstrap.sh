99342ccb36bd95d59a979ed0fa1ed9866c0a2dbb
Marius Gabor <marius.gabor@seco.admin.ch>
2020-01-14 16:36:17 +0100

    DF-1896: First try to deploy ALV Portal to Openshift.

---
+++ openshift/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -m
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-dummy}:x:$(id -u):0:${USER_NAME:-openshift} user:${HOME}:/usr/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
+## TODO check environment specific config server
+# start JHipster Registry
+java $JAVA_OPTS -Dhazelcast.logging.type=slf4j -Dlogging.config="/srv/jobroom/logback-spring.xml" \
+  -server -jar /srv/jobroom/alv-portal.jar \
+  --spring.profiles.active=development,swagger \
+  --spring.cloud.config.uri="http://admin:admin@registry-development.apps.admin.arbeitslosenkasse.ch/config" \
+  --jhipster.registry.password=admin \
+  --spring.cloud.config.label=openshift \
+  --server.port=8080 \
+  --spring.liquibase.dropFirst=False
---
+++ openshift/jenkinsfile.groovy
@@ -0,0 +1,86 @@
+pipeline {
+    agent { label 'maven' }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+        timeout(time: 10, unit: 'MINUTES')
+    }
+
+    environment {
+        DEFAULT_JVM_OPTS = '-Dhttp.proxyHost=l98fppx1.admin.arbeitslosenkasse.ch -Dhttp.proxyPort=8080 -Dhttps.proxyHost=l98fppx1.admin.arbeitslosenkasse.ch -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts="*.admin.arbeitslosenkasse.ch|172.27.97.10|172.27.97.11|172.27.97.12|172.30.0.1"'
+        JAVA_TOOL_OPTIONS = "$JAVA_TOOL_OPTIONS $DEFAULT_JVM_OPTS"
+        SERVER_URL = "https://alvch.jfrog.io/alvch"
+        CREDENTIALS = "artifactory-deploy"
+        FONTAWESOME_NPM_AUTH_TOKEN="F210019D-75E0-4C3D-B1D6-CE6A1B68FBFB"
+        ARTIFACTORY_SERVER = "alv-ch"
+        MAVEN_HOME = "/opt/rh/rh-maven35/root/usr/share/xmvn"
+        SONAR_LOGIN = credentials('SONAR_TOKEN')
+        SONAR_SERVER = "${env.SONAR_HOST_URL}"
+    }
+
+    stages {
+
+        stage('Miau') {
+            steps {
+                sh '''
+                  echo "PATH = ${PATH}"
+                  echo "MAVEN_HOME = ${MAVEN_HOME}"
+              '''
+            }
+        }
+
+        stage('Artifactory configuration') {
+            steps {
+                rtServer(
+                    id: ARTIFACTORY_SERVER,
+                    url: SERVER_URL,
+                    credentialsId: CREDENTIALS
+                )
+
+                rtMavenDeployer(
+                    id: "MAVEN_DEPLOYER",
+                    serverId: ARTIFACTORY_SERVER,
+                    releaseRepo: "libs-releases-local",
+                    snapshotRepo: "libs-snapshots-local"
+                )
+
+                rtMavenResolver(
+                    id: "MAVEN_RESOLVER",
+                    serverId: ARTIFACTORY_SERVER,
+                    releaseRepo: "libs-releases-ocp",
+                    snapshotRepo: "libs-snapshots"
+                )
+            }
+        }
+
+        stage('Exec Maven') {
+            steps {
+                rtMavenRun(
+                    pom: 'pom.xml',
+                    goals: 'clean install package -DskipTests -DskipITs=true -D',
+                    deployerId: "MAVEN_DEPLOYER",
+                    resolverId: "MAVEN_RESOLVER"
+                )
+            }
+        }
+
+        stage('SonarQube') {
+            steps {
+                rtMavenRun(
+                    pom: 'pom.xml',
+                    goals: 'sonar:sonar -Dsonar.projectKey=AlvPortal -Dsonar.host.url="$SONAR_SERVER" -Dsonar.login=$SONAR_LOGIN',
+                    deployerId: "MAVEN_DEPLOYER",
+                    resolverId: "MAVEN_RESOLVER"
+                )
+            }
+        }
+
+        stage('Publish build info') {
+            steps {
+                rtPublishBuildInfo(
+                    serverId: ARTIFACTORY_SERVER
+                )
+            }
+        }
+    }
+}
