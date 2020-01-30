26835d50115cc537c462943d4e0879e824fcb26a
Iurii Katkov <iurii.katkov@mimacom.com>
2020-01-30 13:35:54 +0100

    DF-2071 business exceptions local handler start

--- alv-portal-ui/src/app/competence-catalog/shared/business-exceptions-handler.service.ts
+++ alv-portal-ui/src/app/competence-catalog/shared/business-exceptions-handler.service.ts
@@ -9,7 +9,32 @@ export interface BusinessExceptionToNotification {
   notification: string;
 }
 
-export const defaultExceptionToNotificationMap: BusinessExceptionToNotification[] = [];
+export const defaultExceptionToNotificationMap: BusinessExceptionToNotification[] = [
+  {
+    exception: BusinessExceptionTypes.CANNOT_DELETE_IN_STATUS_PUBLISHED,
+    notification: 'portal.competence-catalog.business-error-messages.cannot_delete_in_status_published'
+  },
+  {
+    exception: BusinessExceptionTypes.CANNOT_PUBLISH_COMPETENCE_SET_WHEN_KNOW_HOW_IS_NOT_PUBLISHED,
+    notification: 'portal.competence-catalog.business-error-messages.cannot_publish_competence_set_when_know_how_is_not_published'
+  },
+  {
+    exception: BusinessExceptionTypes.BFS_CODE_ALREADY_REFERENCED_IN_CH_FICHE,
+    notification: 'portal.competence-catalog.business-error-messages.bfs_code_already_referenced_in_ch_fiche'
+  },
+  {
+    exception: BusinessExceptionTypes.CANNOT_PUBLISH_DRAFT,
+    notification: 'portal.competence-catalog.business-error-messages.cannot_publish_draft'
+  },
+  {
+    exception: BusinessExceptionTypes.CANNOT_UNPUBLISH_KNOW_HOW_REFERENCED_IN_A_PUBLISHED_COMPETENCE_SET,
+    notification: 'portal.competence-catalog.business-error-messages.cannot_unpublish_know_how_referenced_in_a_published_competence_set'
+  },
+  {
+    exception: BusinessExceptionTypes.GENERAL_BUSINESS_EXCEPTION,
+    notification: 'portal.competence-catalog.business-error-messages.general_business_exception'
+  }
+];
 
 let a: keyof typeof BusinessExceptionTypes;
 a = 'BFS_CODE_ALREADY_REFERENCED_IN_CH_FICHE';
