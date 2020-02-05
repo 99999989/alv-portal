2f5a59e52da8c161106fb6aa1e49e9f008462e1d
Emina Stricak <eminastricak@gmail.com>
2020-02-05 13:38:34 +0100

    add collapse-notification component

---
+++ alv-portal-ui/src/app/shared/layout/collapse-notification/collapse-notification.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { collapseExpandAnimation } from '../../animations/animations';
+// Angular animations have problems with animating the transition between display:none and display:block, so we couldn't
+// implement the animation the simple way. Instead, the following hack was used:
+// 1. We only collapse to 1 px, so that no optimizer can remove the panel content from the DOM
+// 2. We use overflow:hidden on a .panel-content container so that when it shrinks, the elements inside are not shown - in css
+// 3.We use min-height: 0 on a .panel-content container to allow schrinking on IE - in css
+// 3. the rest is done in animations below. We purposefully remove paddings on collapsed, because otherwise the top of the first
+// element will be visible inside the closed panel.
+// If you think that you find simpler way to collapsing, please test it thoroughly with IE and Safari.
+@Component({
+  selector: 'alv-collapse-notification',
+  templateUrl: './collapse-notification.component.html',
+  styleUrls: ['./collapse-notification.component.scss'],
+  animations: [collapseExpandAnimation]
+})
+export class CollapseNotificationComponent {
+
+  @Input() panelId: string;
+
+  @Input() panelTitle: string;
+
+  @Input()
+  isCollapsed = false;
+
+  @Output()
+  collapsed = new EventEmitter<boolean>(); // true if isCollapsed===true
+
+  @Input()
+  isAlwaysExpanded = false;
+
+  toggle() {
+    this.isCollapsed ? this.expand() : this.collapse();
+  }
+
+  expand() {
+    this.isCollapsed = false;
+    this.emitCollapseEvent();
+  }
+
+  collapse() {
+    this.isCollapsed = true;
+    this.emitCollapseEvent();
+  }
+
+  private emitCollapseEvent() {
+    this.collapsed.emit(this.isCollapsed);
+  }
+}
